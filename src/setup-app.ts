bff0c49bbf90af0db8de94b8fb7ad29fc0f1eb87
Volodymyr Borshchuk <vovkavovka000@gmail.com>
2025-11-09 21:36:30 +0100

    just test

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -10,8 +10,8 @@ export const setupApp = (app: Express) => {
     res.status(200).send('hello world!!!');
   });
 
-  app.use('/videos', videosRouter);
-  app.use('/testing', testingRouter);
+  app.use('/hometask_01/api/videos', videosRouter);
+  app.use('/hometask_01/api//testing', testingRouter);
 
   setupSwagger(app);
   return app;
